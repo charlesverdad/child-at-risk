e755921b2243ddc26619f0653f9e5296a7716e1c
Charles Verdad <charles@kalibrr.com>
2016-11-05 04:03:16 +0800

    add child model

--- children/models.py
+++ children/models.py
@@ -3,3 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+'''https://docs.djangoproject.com/en/1.10/ref/models/fields/#model-field-types'''
+
+
+class Child(models.Model):
+	firstname = models.CharField(max_length=50)
+	middlename = models.CharField(max_length=50)
+	lastname = models.CharField(max_length=50)
+	nickname = models.CharField(max_length=50)
+	
+	birthday = models.DateField()
+	gender = models.CharField(
+		max_length=2,
+		choices=(('M', 'Male'), ('F', 'female'))
+	)
+	race = models.CharField(max_length=50)
+	religion = models.CharField(max_length=50)
+	nationality = models.CharField(max_length=50)
+	image = models.ImageField()
+
+	# home
