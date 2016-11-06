d9561c673ce0a4567f55d418e48df4f7fed654d9
Marvie Alar <alar.marvie@gmail.com>
2016-11-06 11:16:13 +0800

    added profile view

--- children/views.py
+++ children/views.py
@@ -4,9 +4,13 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def home(request):
+def home2(request):
 	return HttpResponse('Hello')
 
-def home2(request):
-	template = loader.get_template('children/hello.html')
+def home(request):
+	template = loader.get_template('children/index.html')
+	return HttpResponse(template.render(None, request))
+
+def childprofile(request):
+	template = loader.get_template('children/child-profile.html')
 	return HttpResponse(template.render(None, request))
